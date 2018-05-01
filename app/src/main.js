ad574b76403722b5290e589eae688697fce4b5cc
Marco Puccio <mpuccio90@gmail.com>
2018-05-01 01:05:37 +0000

    setup gmaps

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import * as VueGoogleMaps from 'vue2-google-maps';
+
 import AppRoot from '@/components/AppRoot';
+import mapsConf from '@/services/maps';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, mapsConf);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ app/src/services/maps.js
@@ -0,0 +1,6 @@
+export default {
+  load: {
+    key: 'AIzaSyBMq1-nfl0swDQB2qtd2eafkwdIHbI5g0w',
+    libraries: 'places',
+  },
+};
