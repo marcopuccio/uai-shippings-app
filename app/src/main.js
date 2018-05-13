da5f77297fc022d4ae6f8c775b8a90653f825f85
Marco Puccio <mpuccio90@gmail.com>
2018-05-13 17:14:06 +0000

    add and setup vuetify

--- app/src/main.js
+++ app/src/main.js
@@ -1,13 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import * as VueGoogleMaps from 'vue2-google-maps';
 
 import AppRoot from '@/components/AppRoot';
 import mapsConf from '@/services/maps';
 
 Vue.config.productionTip = false;
-
+Vue.use(Vuetify);
 Vue.use(VueGoogleMaps, mapsConf);
 
 /* eslint-disable no-new */
