9b744329c634c31dee71ba751c6e83e2f3686099
Marco Puccio <mpuccio90@gmail.com>
2018-05-14 01:34:30 +0000

    set up api

---
+++ api/Controllers/ShippingsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ShippingsController : Controller
+    {
+        // GET api/shippings
+        [HttpGet]
+        public JsonResult Get()
+        {
+          Dictionary<string, int> defaults = new Dictionary<string, int>
+          {
+              { "maxDistance", 100 },
+              { "kmPrice", 50 }
+          };
+          return Json(defaults);
+        }
+    }
+}
