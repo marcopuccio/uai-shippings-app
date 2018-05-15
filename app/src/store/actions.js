f41e51dba1d09b44d432560ad53c170af22d0e32
Marco Puccio <mpuccio90@gmail.com>
2018-05-15 00:14:10 +0000

    lint

--- app/src/store/actions.js
+++ app/src/store/actions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const isProd = process.env.NODE_ENV === 'production';
 
 const BASE_API_URL = isProd ? 'http://api-shippings.marcopucc.io' : 'http://localhost:5000/api';
-const SHIPPINGS_API_URL =`${BASE_API_URL}/shippings/`;
+const SHIPPINGS_API_URL = `${BASE_API_URL}/shippings/`;
 
 export default {
   setPlace({ commit }, payload) {
