a728b9dce41b71a86b32369485ec64aaace47120
Marco Puccio <mpuccio90@gmail.com>
2018-05-15 00:10:49 +0000

    vary API url by envs

--- app/src/store/actions.js
+++ app/src/store/actions.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
+const isProd = process.env.NODE_ENV === 'production';
+
+const BASE_API_URL = isProd ? 'http://api-shippings.marcopucc.io' : 'http://localhost:5000/api';
+const SHIPPINGS_API_URL =`${BASE_API_URL}/shippings/`;
 
 export default {
   setPlace({ commit }, payload) {
     commit('setPlace', payload);
   },
   fetchShippingsApi({ commit }) {
-    axios.get('http://localhost:5000/api/shippings')
+    axios.get(SHIPPINGS_API_URL)
       .then((response) => {
         const shippingsApi = response.data;
         commit('loadShippinsgApi', { shippingsApi });
