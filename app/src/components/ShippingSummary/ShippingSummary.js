9dce15a0f21a6b4650d72a4dc50679eaebe208f8
Marco Puccio <mpuccio90@gmail.com>
2018-05-22 02:36:49 +0000

    select date flow

--- app/src/components/ShippingSummary/ShippingSummary.js
+++ app/src/components/ShippingSummary/ShippingSummary.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 import { mapGetters } from 'vuex';
 
+import moment from '@/services/moment';
+
 @Component({
   computed: mapGetters([
     'distance',
@@ -10,11 +12,35 @@ import { mapGetters } from 'vuex';
   ]),
 })
 export default class ShippingForm extends Vue {
+  shipment = {
+    date: null,
+    turn: null,
+  };
+
+  datePicker = {
+    fullWidth: true,
+    min: moment().format(),
+    reactive: true,
+    headerColor: 'red darken-3',
+  }
+
   get kmPrice() {
     return this.shippingsApi.kmPrice;
   }
 
-  get cantShip() {
-    return this.distance > this.shippingsApi.maxDistance;
+  get hasMinDistance() {
+    return this.distance > 0;
+  }
+
+  get hasMaxDistance() {
+    return this.shippingsApi.maxDistance < this.distance;
+  }
+
+  get canSelectDate() {
+    return this.hasMinDistance && !this.hasMaxDistance;
+  }
+
+  get isValidShipping() {
+    return this.shipment.date && this.shipment.turn;
   }
 }
