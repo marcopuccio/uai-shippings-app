7fa1a25e580894d3ac347936691dec01a0ef6657
Marco Puccio <mpuccio90@gmail.com>
2018-05-22 03:19:06 +0000

    clear shipping date & turn when place changes

--- app/src/store/mutations.js
+++ app/src/store/mutations.js
@@ -1,6 +1,8 @@
 export default {
   setPlace(state, { place }) {
     state.place = place;
+    // clear shipping date & turn
+    state.shipping = { date: null, turn: null };
   },
   setShippingDate(state, { date }) {
     state.shipping.date = date;
